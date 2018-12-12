16767caf4322acf27655fb5882863a234f10a05f
DeanTevin <Deantevinn@gmail.com>
2018-12-12 15:32:34 +0700

    Task2 First Commit

--- project2/UrlChecker.java
+++ project2/UrlChecker.java
@@ -1,3 +1,15 @@
+package chapter02;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class UrlChecker{
+	public static void main(String args[]){
+		Pattern Mypattern = Pattern.compile("^((https?|ftp)\\.)[a-z0-9-]+(\\.[a-z-0-9-]+[a-z]+)+([/?].*)?$");
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		Matcher Mymatcher = Mypattern.matcher(name);
+		Boolean Myboolean = Mymatcher.matches();
 if(Myboolean == true) {
 	System.out.println("Url is correct");
 	}
